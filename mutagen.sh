ebe6939bc6c45b4422f4e1891a0f5aa9e6835f5f
csaborio <csaborio@scorpiotek.com>
2021-08-10 18:43:38 +1000

    Modified compose file to launch website on port 80

--- mutagen.sh
+++ mutagen.sh
@@ -1,5 +1,5 @@
 mutagen sync create --sync-mode=one-way-safe --default-owner-beta=www-data \
---name=MUTAGEN_SYNC_NAME \
-~/WebSites/YOUR_WEBSITE_DIR/wp-content docker://DOCKER_CONTAINER_NAME/var/www/html/wp-content
+--name=vc-dev \
+~/WebSites/connect.vinnies.org.au/wp-content/mu-plugins/ docker://vc_dev/var/www/html/wp-content/mu-plugins
 
 mutagen sync monitor
