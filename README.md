b32d33dd58fb70bc183afcbf4e453989d7b7c482
csaborio <csaborio@scorpiotek.com>
2021-08-28 18:01:52 +1000

    Mutagen script correctly loads values

--- README.md
+++ README.md
@@ -159,3 +159,4 @@ This script will:
 * Added environment variables and a way for scripts to load them 
 * changed copyDistFolder to hold ,env variables
 * Compatible with mySQL workaround for M1 chips
+* Mutagen script correctly loads values
--- scripts/mutagen.sh
+++ scripts/mutagen.sh
@@ -2,8 +2,8 @@
 export $(grep -v '^#' .env | xargs)
 
 mutagen sync create --sync-mode=one-way-safe --default-owner-beta=www-data --default-group-beta=www-data \
---name=$COMPOSE_PROJECT_NAME \
+--name="${MUTAGEN_SYNC_NAME}" \
 --ignore=dist,tests,node_modules,config,process,tests_output,package-lock.json,package.json \
-~/Websites/$WEBSITE_FOLDER_NAME/wp-content/mu-plugins docker://$COMPOSE_PROJECT_NAME/var/www/html/wp-content/mu-plugins
+~/Websites/"${WEBSITE_FOLDER_NAME}"/wp-content/mu-plugins docker://"${COMPOSE_PROJECT_NAME}"/var/www/html/wp-content/mu-plugins
 
-mutagen sync monitor
+mutagen sync monitor "${MUTAGEN_SYNC_NAME}"
