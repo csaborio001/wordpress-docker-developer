19b3c559c047424b179e0867c2572ba93ec7536f
Christian Saborio <csaborio001@users.noreply.github.com>
2024-10-24 17:49:37 +1100

    Update halt.sh

--- scripts/halt.sh
+++ scripts/halt.sh
@@ -2,11 +2,14 @@
 export $(grep -v '^#' ../.env | xargs)
 
 pgrep gulp | xargs -I{} -n 1 kill {}
-docker cp "${COMPOSE_PROJECT_NAME}":/root/.zsh_history xdebug/files-to-copy/root
-rm -R ../wp-content/plugins.bkp
-rm -R ../wp-content/uploads.bkp
-mv ../wp-content/plugins ../wp-content/plugins.bkp
-mv ../wp-content/uploads ../wp-content/uploads.bkp
+docker cp "${COMPOSE_PROJECT_NAME}":/root/.zsh_history config/files-to-copy/root
+#docker exec -it "${COMPOSE_PROJECT_NAME}" rsync -a --exclude '.git' --exclude '**/node_modules/*' /var/www/html/wp-content /var/www/html/wp-content-master
+#rm -R ../wp-content/plugins.bkp
+#rm -R ../wp-content/uploads.bkp
+#mv ../wp-content/plugins ../wp-content/plugins.bkp
+#mv ../wp-content/uploads ../wp-content/uploads.bkp
 docker cp "${COMPOSE_PROJECT_NAME}":/var/www/html/wp-content/plugins ../wp-content
 docker cp "${COMPOSE_PROJECT_NAME}":/var/www/html/wp-content/uploads ../wp-content
 docker-compose down
+
+
